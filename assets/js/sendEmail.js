8a93ee247e7508ffbcbe350a4a26788ac1312062
Michelle Clement <michellelclement@gmail.com>
2021-11-17 20:13:16 +0000

    Update add venue form details + try connect email.js to contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,11 @@
+function sendMail(contactForm) {
+    emailjs
+      .send("gmail", "baby_change", {
+        from_name: contactForm.contactName.value,
+        from_email: contactForm.contactEmail.value,
+        message: contactForm.message.value,
+      })
+      .then(alert("Thank you! You email has been sent"));
+  
+    $("#contact-form").text();
+  }
